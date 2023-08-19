1bbf02014c576ec596c882380147d3af21d47330
Michael Fuerst <mail@michaelfuerst.de>
2023-08-19 20:56:03 +0200

    Rework popup to be just a popup not specialized for settings.

---
+++ icons/icons.ts
@@ -0,0 +1,5 @@
+import barsURL  from  "./bars-solid.svg"
+import xmarkURL  from  "./xmark-solid.svg"
+
+export let bars = "<image src='" + barsURL + "' />";
+export let xmark = "<image src='" + xmarkURL + "' />";
--- popup.ts
+++ popup.ts
@@ -1,45 +1,76 @@
 import { Button } from "./form";
+import { xmark } from "./icons/icons";
 import { Module } from "./module";
-import barsURL  from  "./icons/bars-solid.svg"
-import xmarkURL  from  "./icons/xmark-solid.svg"
 
-let bars = "<image src='" + barsURL + "' />";
-let xmark = "<image src='" + xmarkURL + "' />";
 
-export class SettingsPopup extends Module<HTMLDivElement> {
-    public toggleButton: Button
+export class Popup extends Module<HTMLDivElement> {
     public container: Module<HTMLDivElement>
 
     public constructor(
         innerClass: string,
-        containerClass: string,
-        toggleClass: string
+        containerClass: string
     ) {
         super("div")
         this.setClass(innerClass)
         this.container = new Module("div")
         this.container.setClass(containerClass)
         this.container.add(this)
-        this.container.hide()
-        this.toggleButton = new Button(bars, toggleClass)
-        this.toggleButton.onClick = this.toggleVisibility.bind(this)
+        document.getElementById("global")?.appendChild(this.container.htmlElement)
     }
 
-    public addToDivById(id: string) {
-        document.getElementById(id)?.appendChild(this.container.htmlElement)
-        document.getElementById(id)?.appendChild(this.toggleButton.htmlElement)
+    public dispose() {
+        document.getElementById("global")?.removeChild(this.container.htmlElement)
     }
 
     public update() {}
+}
 
-    public toggleVisibility() {
-        if (this.container.isVisible()) {
-            this.container.hide()
-            this.toggleButton.htmlElement.innerHTML = bars
-        } else {
-            this.toggleButton.htmlElement.innerHTML = xmark
-            this.update()
-            this.container.show()
+
+export class ConfirmCancelPopup extends Popup {
+    public constructor(
+        innerClass: string,
+        containerClass: string,
+        question: string,
+        confirmText: string,
+        cancelText: string
+    ) {
+        super(innerClass, containerClass)
+        this.add(new Module("p", question))
+        let confirm = new Button(confirmText)
+        confirm.onClick = () => {
+            this.dispose()
+            this.onConfirm()
         }
+        this.add(confirm)
+        let cancel = new Button(cancelText)
+        cancel.onClick = () => {
+            this.dispose()
+            this.onCancel()
+        }
+        this.add(cancel)
     }
+
+    public onConfirm () {
+        console.log("ConfirmCancelPopup::onConfirm not implemented. Must be implemented by subclass.")
+    }
+
+    public onCancel() {
+        console.log("ConfirmCancelPopup::onCancel not implemented. Must be implemented by subclass.")
+    }
+}
+
+
+export class ExitablePopup extends Popup {
+    public constructor(
+        innerClass: string,
+        containerClass: string,
+        exitButtonClass: string = ""
+    ) {
+        super(innerClass, containerClass)
+        let exitButton = new Button(xmark, exitButtonClass)
+        exitButton.onClick = this.dispose.bind(this)
+        this.add(exitButton)
+    }
+
+    public update() {}
 }
